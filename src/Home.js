a353531db68de6ca459bc334b15f93caf00d55fd
saransh <sethia.saransh.ss@gmail.com>
2024-11-08 12:40:56 +0530

    tenth commit

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   const [states, setStates] = useState([]);
   const [stateName, setStateName] = useState("");
   const [cities, setCities] = useState([]);
-  const [cityName, setCityName] = useState("")
+  const [cityName, setCityName] = useState("");
   const [loading, setLoading] = useState(false);
   const [isCountrySelected, setIsCountrySelected] = useState(false);
   const [isStateSelected, setIsStateSelected] = useState(false);
@@ -17,36 +17,41 @@ const Home = () => {
     const getCountryName = e.target.value;
     console.log("country-name", getCountryName);
     setCountryName(getCountryName);
-    setIsCountrySelected(true)
+    setIsCountrySelected(true);
   };
 
   const handleState = (e) => {
     const getStateName = e.target.value;
     console.log("state-name", getStateName);
     setStateName(getStateName);
-    setIsStateSelected(true)
+    setIsStateSelected(true);
   };
 
   const handleCity = (e) => {
     const getCityName = e.target.value;
     setCityName(getCityName);
-    setIsCitySelected(true)
-  }
+    setIsCitySelected(true);
+  };
+
+  // if (isCitySelected === true) {
+  //   setIsCountrySelected(false);
+  //   setIsStateSelected(false);
+  // }
 
   useEffect(() => {
     const fetchCountries = async () => {
-        try{
-            setLoading(true);
-            const response = await axios.get(
-              "https://crio-location-selector.onrender.com/countries"
-            );
-      
-            const data = await response.data;
-            setCountries(await data);
-            setLoading(false);
-        }catch(error){
-            console.log('error-1',error)
-        }
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://crio-location-selector.onrender.com/countries"
+        );
+
+        const data = await response.data;
+        setCountries(await data);
+        setLoading(false);
+      } catch (error) {
+        console.log("error-1", error);
+      }
     };
 
     fetchCountries();
@@ -54,77 +59,81 @@ const Home = () => {
 
   useEffect(() => {
     const fetchStates = async (countryName) => {
-        try{
-            if(isCountrySelected === true){
-              setLoading(true);
-                const response = await axios.get(
-                    `https://crio-location-selector.onrender.com/country=${countryName}/states`
-                  );
-                  const data = await response.data;
-                  setStates(await data);
-                  setLoading(false);
-            } else {
-                return;
-            }
-
-        }catch(error){
-            console.log('error-2',error)
+      try {
+        if (isCountrySelected === true) {
+          setLoading(true);
+          const response = await axios.get(
+            `https://crio-location-selector.onrender.com/country=${countryName}/states`
+          );
+          const data = await response.data;
+          setStates(await data);
+          setLoading(false);
+        } else {
+          return;
         }
-        
-
+      } catch (error) {
+        console.log("error-2", error);
+      }
     };
     fetchStates(countryName);
   }, [countryName]);
 
   useEffect(() => {
     const fetchCities = async (countryName, stateName) => {
-        try{
-            if(isStateSelected === true){
-              setLoading(true);
-                const response = await axios.get(
-                    `https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`
-                  );
-                  const data = await response.data;
-                  setCities(await data);
-                  setLoading(false);
-            } else {
-                return;
-            }
-
-        }catch(error){
-            console.log('error-3',error)
+      try {
+        if (isStateSelected === true) {
+          setLoading(true);
+          const response = await axios.get(
+            `https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`
+          );
+          const data = await response.data;
+          setCities(await data);
+          setLoading(false);
+        } else {
+          return;
         }
-        
-
+      } catch (error) {
+        console.log("error-3", error);
+      }
     };
     fetchCities(countryName, stateName);
   }, [countryName, stateName]);
 
   return (
     <div>
-    <h1>Select Location</h1>
-      <select onChange={(e) => handleCountry(e)} value={countryName}>
+      <h1>Select Location</h1>
+      <select 
+      disabled = {isCitySelected && isStateSelected ? true : false}
+      onChange={(e) => handleCountry(e)} value={countryName}>
         <option>Select Country</option>
         {countries.map((country, id) => (
           <option key={id}>{country}</option>
         ))}
       </select>
 
-      <select disabled={isCountrySelected ? false : true} onChange={(e) => handleState(e)} value={stateName}>
-       <option>Select State</option>
+      <select
+        disabled={isCountrySelected && !isStateSelected ? false : true}
+        onChange={(e) => handleState(e)}
+        value={stateName}
+      >
+        <option>Select State</option>
         {states.map((state, id) => (
           <option key={id}>{state}</option>
         ))}
       </select>
 
-      <select disabled={isStateSelected ? false : true} onChange={(e) => handleCity(e)} value={cityName}>
+      <select
+        disabled={isStateSelected && !isCitySelected ? false : true}
+        onChange={(e) => handleCity(e)}
+        value={cityName}
+      >
         <option>Select City</option>
         {cities.map((city, id) => (
           <option key={id}>{city}</option>
         ))}
       </select>
       {isCitySelected && (
-        <p>{`You selected ${cityName}, ${stateName}, ${countryName}`}</p>
+        <h5>{`You selected ${cityName}, ${stateName}, ${countryName}`}</h5>
       )}
     </div>
   );
